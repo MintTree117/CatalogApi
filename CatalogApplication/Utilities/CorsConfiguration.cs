40e7d0e361dda601b6b5f4fb03251953dee3054a
MintTree117 <135687103+MintTree117@users.noreply.github.com>
2024-07-26 20:05:25 -0400

    More cors testing.

--- CatalogApplication/Utilities/CorsConfiguration.cs
+++ CatalogApplication/Utilities/CorsConfiguration.cs
@@ -6,10 +6,12 @@ internal static class CorsConfiguration
     {
         var origins = builder.Configuration.GetSection( "AllowedOrigins" ).Get<string[]>() 
             ?? throw new Exception( "Failed to get AllowedOrigins from configuration during startup." );
+
+        string[] testOrigins = ["https://martinsorderingapi.azurewebsites.net", "https://martinscatalogapi.azurewebsites.net", "https://happy-bush-0b0f3e80f.5.azurestaticapps.net"];
         
         builder.Services.AddCors( options => {
             options.AddDefaultPolicy( cors => cors
-                .WithOrigins( origins )
+                .WithOrigins( testOrigins )
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials() );
